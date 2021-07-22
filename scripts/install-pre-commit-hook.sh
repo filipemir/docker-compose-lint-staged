087ecbf4fe6d0440f37dead85c08532e738508f9
Filipe Miranda <filipe.miranda@buoyhealth.com>
2021-07-22 14:20:45 -0400

    Test

--- scripts/install-pre-commit-hook.sh
+++ scripts/install-pre-commit-hook.sh
@@ -3,10 +3,13 @@ echo "Installing pre-commit hook:"
 
 # Absolute path to this script, e.g. /home/user/bin/foo.sh
 SCRIPT=$(readlink -f "$0")
+echo $SCRIPT
 
-cd /tmp/.githooks 
-ls | xargs chmod +x 
-cd /tmp/.git/hooks
-find /tmp/.githooks -type f -exec ln -sf {} /tmp/.git/hooks/ \;
+# cd /tmp/.githooks 
+# ls -la
+# ls | xargs chmod +x 
+cp /release/scripts/pre-commit-hook.sh /tmp/.git/hooks/pre-commit
+# cd /tmp/.git/hooks
+# find /tmp/.githooks -type f -exec ln -sf {} /tmp/.git/hooks/ \;
 echo 'githooks installed'"
 
